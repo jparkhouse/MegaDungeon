9938f816546ba16dd3891dc89229941ed83d96bc
Jake Parkhouse <jparkhouse1511@gmail.com>
2025-01-12 18:34:14 +0000

    fixed current health bug\nadded comments to functions\nremoved duplicated alive property

--- models/character.gd
+++ models/character.gd
@@ -3,25 +3,37 @@ extends Resource
 class_name Character
 
 var name: String
-var alive: bool
 var attack: Attack
 var state: CharacterAction
 var state_time_remaining: int
 var max_health: int
 var curr_health: int
 
+func _init(name: String, max_health: int):
+	self.name = name
+	self.max_health = max_health
+	self.curr_health = max_health
+	self.state = CharacterAction.new()
+	
+	
+'''
+Applies some amount of damage to the character
+'''
 func apply_damage(amount: int):
-	self.health -= amount
-	if self.health <= 0:
+	self.curr_health -= amount
+	if self.curr_health <= 0:
 		die()
 
+'''
+Kills the character
+'''
 func die():
-	self.alive = false
-	self.health = 0
+	self.curr_health = 0
 	self.state.die()
 
+'''
+Returns the current action and how long is remaining
+'''
 func get_current_action() -> CharacterAction:
 	return self.state
-
-
 
