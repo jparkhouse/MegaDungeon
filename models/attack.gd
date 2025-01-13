8ad53c8b9bd2a6cc0638622a3ea2eb458791dfaf
Jake Parkhouse <jparkhouse1511@gmail.com>
2025-01-13 17:39:41 +0000

    fixed potential variable shadowing bug

--- models/attack.gd
+++ models/attack.gd
@@ -5,7 +5,7 @@ class_name Attack
 var speed: int
 var damage: int
 
-func _init(speed = 5, damage = 1):
-	self.speed = speed
-	self.damage = damage
+func _init(attack_speed = 5, attack_damage = 1):
+	self.speed = attack_speed
+	self.damage = attack_damage
 	
